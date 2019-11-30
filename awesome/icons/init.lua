6a86b834f4afc31b5a90d6876721f1a15d4fb487
William McKinnon <willpower3309@gmail.com>
2019-11-30 11:46:20 -0500

    added tag icons

--- awesome/icons/init.lua
+++ awesome/icons/init.lua
@@ -7,22 +7,22 @@
 
 
 -- define icon folder directory
-local dir = os.getenv('HOME') .. '/.config/awesome/icons'
+local dir = os.getenv('HOME') .. '/.config/awesome/icons/'
 
 -- return icons
 return {
-  logout = dir .. '/logout.svg',
-  sleep = dir .. '/power-sleep.svg',
-  power = dir .. '/power.svg',
-  lock = dir .. '/lock.svg',
-  brightness = dir .. '/brightness.svg',
-  volume = dir .. '/volume.svg',
-  chart = dir .. '/chart.svg',
-  memory = dir .. '/memory.svg',
-  harddisk = dir ..'/harddisk.svg',
-  thermometer = dir .. '/thermometer.svg',
-  menu = dir .. '/menu.svg',
-  search = dir .. '/search.svg',
-  close = dir .. '/close.png',
-  restart = dir .. '/restart.svg'
+  logout = dir .. 'logout.svg',
+  sleep = dir .. 'power-sleep.svg',
+  power = dir .. 'power.svg',
+  lock = dir .. 'lock.svg',
+  brightness = dir .. 'brightness.svg',
+  volume = dir .. 'volume.svg',
+  chart = dir .. 'chart.svg',
+  memory = dir .. 'memory.svg',
+  harddisk = dir ..'harddisk.svg',
+  thermometer = dir .. 'thermometer.svg',
+  menu = dir .. 'menu.svg',
+  search = dir .. 'search.svg',
+  close = dir .. 'close.png',
+  restart = dir .. 'restart.svg'
 }
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -57,31 +57,103 @@ awful.layout.layouts = {
 -- ===================================================================
 
 
-awful.screen.connect_for_each_screen(function(s)
-    -- Each screen has its own tag table.
-    local l = awful.layout.suit -- Alias to save time :)
-    -- Tag layouts
-    local layouts = {
-        l.tile,
-        l.tile,
-        l.tile,
-        l.tile,
-        l.tile,
-        l.tile,
-        l.tile,
-        l.tile,
-        l.tile,
-        l.tile
-    }
+-- define icon folder directory
+local dir = os.getenv('HOME') .. '/.config/awesome/icons/tags/'
 
-    -- Tag names
-    local tagnames = beautiful.tagnames or { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }
-    -- Create all tags at once (without seperate configuration for each tag)
-    awful.tag(tagnames, s, layouts)
+local tags = {
+  {
+    icon = dir .. 'terminal.svg',
+    type = 'terminal',
+    defaultApp = 'kitty',
+    screen = 1
+  },
+  {
+    icon = dir .. 'google-chrome.svg',
+    type = 'chrome',
+    defaultApp = 'firefox',
+    screen = 1
+  },
+  {
+    icon = dir .. 'code-braces.svg',
+    type = 'code',
+    defaultApp = 'atom',
+    screen = 1
+  },
+  {
+    icon = dir .. 'folder.svg',
+    type = 'files',
+    defaultApp = 'nemo',
+    screen = 1
+  },
+  {
+    icon = dir .. 'music.svg',
+    type = 'music',
+    defaultApp = 'kitty -e ncmpcpp',
+    screen = 1
+  },
+  {
+    icon = dir .. 'google-controller.svg',
+    type = 'game',
+    defaultApp = 'supertuxkart',
+    screen = 1
+  },
+  {
+    icon = dir .. 'art.svg',
+    type = 'art',
+    defaultApp = 'gimp',
+    screen = 1
+  },
+  {
+    icon = dir .. 'vbox.svg',
+    type = 'virtualbox',
+    defaultApp = 'virtualbox',
+    screen = 1
+  },
+  {
+    icon = dir .. 'flask.svg',
+    type = 'any',
+    defaultApp = '',
+    screen = 1
+  }
+}
+
+awful.layout.layouts = {
+  awful.layout.suit.spiral.dwindle,
+  awful.layout.suit.tile,
+  awful.layout.suit.max
+}
+
+
+screen.connect_signal("request::desktop_decoration", function(s)
+  for i, tag in pairs(tags) do
+    awful.tag.add(
+      i,
+      {
+        icon = tag.icon,
+        icon_only = true,
+        layout = awful.layout.suit.spiral.dwindle,
+        gap_single_client = false,
+        gap = 4,
+        screen = s,
+        defaultApp = tag.defaultApp,
+        selected = i == 1
+      }
+    )
+  end
 end)
 
--- Determines how floating clients should be placed
-local client_placement_f = awful.placement.no_overlap + awful.placement.no_offscreen
+
+_G.tag.connect_signal(
+  'property::layout',
+  function(t)
+    local currentLayout = awful.tag.getproperty(t, 'layout')
+    if (currentLayout == awful.layout.suit.max) then
+      t.gap = 0
+    else
+      t.gap = 4
+    end
+  end
+)
 
 
 -- ===================================================================
