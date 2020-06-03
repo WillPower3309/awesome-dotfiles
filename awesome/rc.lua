1d246dd3454b5524888c7005b10b58b661362ff5
William McKinnon <contact@willmckinnon.com>
2020-06-03 03:42:48 -0400

    Fixed autofocusing issues

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -112,8 +112,9 @@ client.connect_signal("manage", function (c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
+require("awful.autofocus")
 client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = false})
+    c:emit_signal("request::activate", "mouse_enter", {raise = true})
 end)
 
 
