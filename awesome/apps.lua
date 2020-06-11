29cce04d1620b5b07816060f390a1991392b8c33
William McKinnon <mail@willmckinnon.com>
2020-06-11 13:50:51 -0400

    Replaced single quotes with double quotes

--- awesome/apps.lua
+++ awesome/apps.lua
@@ -6,8 +6,8 @@
 --      ╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝
 
 
-local awful = require('awful')
-local filesystem = require('gears.filesystem')
+local awful = require("awful")
+local filesystem = require("gears.filesystem")
 
 -- define module table
 local apps = {}
@@ -43,11 +43,11 @@ local run_on_start_up = {
 function apps.autostart()
    for _, app in ipairs(run_on_start_up) do
       local findme = app
-      local firstspace = app:find(' ')
+      local firstspace = app:find(" ")
       if firstspace then
          findme = app:sub(0, firstspace - 1)
       end
-         awful.spawn.with_shell(string.format('pgrep -u $USER -x %s > /dev/null || (%s)', findme, app), false)
+         awful.spawn.with_shell(string.format("pgrep -u $USER -x %s > /dev/null || (%s)", findme, app), false)
    end
 end
 
