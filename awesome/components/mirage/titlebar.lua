52249253858555e7f26d19218350574881ad986f
Will McKinnon <services@willmckinnon.com>
2020-10-10 17:53:51 -0400

    fine tuned titlebar sizing

--- awesome/components/mirage/titlebar.lua
+++ awesome/components/mirage/titlebar.lua
@@ -4,12 +4,16 @@ local dpi = require("beautiful").xresources.apply_dpi
 
 -- Add a titlebar
 client.connect_signal("request::titlebars", function(c)
-   awful.titlebar(c) : setup {
+   local titlebar = awful.titlebar(c, {
+      size = dpi(30)
+   })
+
+   titlebar: setup {
       {
          -- AwesomeWM native buttons (images loaded from theme)
-         wibox.layout.margin(awful.titlebar.widget.closebutton(c), dpi(8), dpi(3), dpi(3), dpi(3)),
-         wibox.layout.margin(awful.titlebar.widget.minimizebutton(c), dpi(3), dpi(3), dpi(3), dpi(3)),
-         wibox.layout.margin(awful.titlebar.widget.maximizedbutton(c), dpi(3), dpi(3), dpi(3), dpi(3)),
+         wibox.layout.margin(awful.titlebar.widget.closebutton(c), dpi(11), dpi(5), dpi(5), dpi(5)),
+         wibox.layout.margin(awful.titlebar.widget.minimizebutton(c), dpi(4), dpi(5), dpi(5), dpi(5)),
+         wibox.layout.margin(awful.titlebar.widget.maximizedbutton(c), dpi(4), dpi(5), dpi(5), dpi(5)),
          layout = wibox.layout.fixed.horizontal
       },
       {
